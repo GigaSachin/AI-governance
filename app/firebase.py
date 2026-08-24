da46342710479bad94f78d3d8f109ca45a3d28e8
Sachin <sachinsingh280106@gmail.com>
2026-08-24 16:30:39 +0530

    Configure Firebase for production

--- app/firebase.py
+++ app/firebase.py
@@ -1,3 +1,5 @@
+import os
+import json
 import firebase_admin
 
 from firebase_admin import credentials
@@ -9,9 +11,19 @@ from firebase_admin import storage
 # FIREBASE INITIALIZATION
 # =========================================================
 
-cred = credentials.Certificate(
-    "serviceAccountKey.json"
-)
+firebase_json = os.getenv("FIREBASE_SERVICE_ACCOUNT_JSON")
+
+if firebase_json:
+    # Render / production
+    service_account_info = json.loads(firebase_json)
+    cred = credentials.Certificate(service_account_info)
+
+else:
+    # Local development
+    cred = credentials.Certificate(
+        "serviceAccountKey.json"
+    )
+
 
 firebase_admin.initialize_app(
     cred,
