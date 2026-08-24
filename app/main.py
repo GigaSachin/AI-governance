c52e95b0938ce6dd11ec9636e9849db5d58c26d2
Sachin <sachinsingh280106@gmail.com>
2026-08-24 18:22:31 +0530

    Fix CORS for Vercel frontend

--- app/main.py
+++ app/main.py
@@ -34,6 +34,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:5500",
         "http://127.0.0.1:5500",
+        "https://aidigitalgovernance0028-weld.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
